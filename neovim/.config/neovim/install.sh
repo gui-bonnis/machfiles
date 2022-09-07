fe88d15061254146e6dab4ca036bdfaefe1fe766
gui-bonnis <guibonnis@gmail.com>
2022-09-07 15:40:39 +0000

    Add neovim installation script

--- neovim/.config/neovim/install.sh
+++ neovim/.config/neovim/install.sh
@@ -8,6 +8,8 @@ sudo apt-get install ninja-build gettext libtool libtool-bin autoconf automake c
 # Arch linux
 sudo pacman -S base-devel cmake unzip ninja tree-sitter curl
 
+git clone https://github.com/neovim/neovim.git
+cd neovim
 
 # Compiling
 make CMAKE_BUILD_TYPE=Release
