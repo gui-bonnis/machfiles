8f29a9856b6bc5fdded7e757b844949c6ce6ab4b
gui-bonnis <guibonnis@gmail.com>
2022-09-07 17:11:56 +0000

    Add installation softwares for neovim

--- neovim/.config/neovim/install.sh
+++ neovim/.config/neovim/install.sh
@@ -2,12 +2,18 @@
 
 # Ubuntu
 sudo apt-get install ninja-build gettext libtool libtool-bin autoconf automake cmake g++ pkg-config unzip curl doxygen
-
-
+sudo apt-get install xsel
+sudo apt-get install ripgrep
+sudo apt install python3-pip
 
 # Arch linux
 sudo pacman -S base-devel cmake unzip ninja tree-sitter curl
+sudo pacman -S xsel
+sudo pacman -S ripgrep
+sudo pacman -S python-pip
 
+
+# Clonning Neovim repository
 cd ~/.config
 git clone https://github.com/neovim/neovim.git
 cd neovim
