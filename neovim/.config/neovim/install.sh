4dfd98be8996edcd8dd3a2fce2e9c1d611b8aaa1
gui-bonnis <guibonnis@gmail.com>
2022-09-07 21:27:54 +0000

    Add installations for neovim

--- neovim/.config/neovim/install.sh
+++ neovim/.config/neovim/install.sh
@@ -5,13 +5,23 @@ sudo apt-get install ninja-build gettext libtool libtool-bin autoconf automake c
 sudo apt-get install xsel
 sudo apt-get install ripgrep
 sudo apt install python3-pip
+sudo apt install fd-find
+
+# Install NodeJS at Ubuntu
+curl -fsSL https://deb.nodesource.com/setup_18.x | sudo -E bash -
+sudo apt-get install -y nodejs
+
+# Install Node at Ubuntu 20.04
+sudo apt update
+sudo apt install nodejs npm
 
 # Arch linux
 sudo pacman -S base-devel cmake unzip ninja tree-sitter curl
 sudo pacman -S xsel
 sudo pacman -S ripgrep
 sudo pacman -S python-pip
-
+sudo pacman -S fd
+sudo pacman -S nodejs npm
 
 # Clonning Neovim repository
 cd ~/.config
@@ -24,6 +34,8 @@ sudo make install
 
 # Add python plugin support
 pip install pynvim
+pip install ueberzug
 
+sudo npm install -g neovim
 
 
