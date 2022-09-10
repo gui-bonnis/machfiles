5d156f099c9d22e4889cc7816da10aea135249de
gui-bonnis <guibonnis@gmail.com>
2022-09-10 19:45:48 +0000

    Add java extentions installation

--- install/java_install/install.sh
+++ install/java_install/install.sh
@@ -11,3 +11,22 @@ sdk version
 
 sdk install java 17.0.4.1-tem
 sdk install maven 3.8.6
+
+
+# Java Debugging and Testing
+# inside nvim directory
+mkdir java-extentions
+cd java-extentions
+
+git clone git@github.com:microsoft/java-debug.git
+cd java-debug/
+./mvnw clean install
+cd ..
+
+git clone git@github.com:microsoft/vscode-java-test.git
+cd vscode-java-test
+npm install
+npm run build-plugin
+cd ..
+
+paru -S google-java-format-bin
