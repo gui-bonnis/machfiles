af441203a73f301e181cc4425ed62d1aec8ac4c2
gui-bonnis <guibonnis@gmail.com>
2022-09-10 17:09:52 +0000

    Install SDK Man and Java 17

---
+++ java_install/install.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+
+# Prerequirement
+sudo pacman -S zip
+
+# Install SDKMan
+curl -s "https://get.sdkman.io" | bash
+source "$HOME/.sdkman/bin/sdkman-init.sh"
+sdk version
+
+sdk install java 17.0.4.1-tem
