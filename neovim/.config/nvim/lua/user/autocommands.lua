737b02d4003215980a8c44c92efcac6488d7048d
gui-bonnis <guibonnis@gmail.com>
2022-09-11 22:49:53 +0000

    Change statusbar size

--- neovim/.config/nvim/lua/user/autocommands.lua
+++ neovim/.config/nvim/lua/user/autocommands.lua
@@ -15,7 +15,7 @@ vim.api.nvim_create_autocmd({ "User" }, {
   callback = function()
     vim.cmd [[
       set showtabline=0 | autocmd BufUnload <buffer> set showtabline=2
-      set laststatus=0 | autocmd BufUnload <buffer> set laststatus=3
+      set laststatus=0 | autocmd BufUnload <buffer> set laststatus=2
     ]]
   end,
 })
